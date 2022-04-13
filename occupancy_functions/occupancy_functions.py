cb1f9996fb8267910727d9f213df8af275f969fe
Justin DiEmmanuele <justindiemmanuele@Justins-Air.attlocal.net>
2022-04-12 22:03:15 -0400

    support for metadata dir

--- occupancy_functions/occupancy_functions.py
+++ occupancy_functions/occupancy_functions.py
@@ -3,7 +3,7 @@ import copy
 import json
 
 
-def create_config(occupancy_template, months_into_future, s3_output_path):
+def create_config(occupancy_template, months_into_future, s3_output_path, metadata_path):
     current_month = datetime.datetime.now().month
     current_year = datetime.datetime.now().year
     config = copy.deepcopy(occupancy_template)
@@ -11,11 +11,12 @@ def create_config(occupancy_template, months_into_future, s3_output_path):
     config["request_config"]["variables"]["request"]["month"] = current_month
     config["request_config"]["variables"]["request"]["year"] = current_year
     config['out_config']['s3_path'] = s3_output_path
+    config['metadata_config']['s3_path'] = metadata_path
     return config
 
 
-def create_occupancy_configs(occupancy_template, months_into_future, out_path, s3_output_path):
-    configs = [create_config(occupancy_template, months_into_future, s3_output_path)]
+def create_occupancy_configs(occupancy_template, months_into_future, out_path, s3_output_path, metadata_path):
+    configs = [create_config(occupancy_template, months_into_future, s3_output_path, metadata_path)]
     configs = {"configs": configs}
     with open(out_path, "w") as f:
         json.dump(configs, f, indent=4)
--- price_functions/price_functions.py
+++ price_functions/price_functions.py
@@ -20,19 +20,20 @@ def get_config_days(days_into_future, skip_days):
         return get_dates_from_days_in_future([i for i in range(days_into_future)])
 
 
-def get_day_config(config_template, start_day, end_day, s3_output_path):
+def get_day_config(config_template, start_day, end_day, s3_output_path, metadata_path):
     config_template = copy.deepcopy(config_template)
     config_template["request_config"]["variables"]["pdpSectionsRequest"]["checkIn"] = start_day
     config_template["request_config"]["variables"]["pdpSectionsRequest"]["checkOut"] = end_day
     config_template["out_config"]["s3_path"] = s3_output_path
+    config_template["metadata_config"]["s3_path"] = metadata_path
     return config_template
 
 
-def create_price_configs(config_template, skip_days, days_into_future, s3_output_path, out_path):
+def create_price_configs(config_template, skip_days, days_into_future, s3_output_path, out_path, metadata_path):
     pricing_configs = []
     days_to_config = get_config_days(days_into_future, skip_days)
     for start, end in zip(days_to_config[:-1], days_to_config[1:]):
-        pricing_configs.append(get_day_config(config_template, start, end, s3_output_path))
+        pricing_configs.append(get_day_config(config_template, start, end, s3_output_path, metadata_path))
 
     configs = {"configs": pricing_configs}
     with open(out_path, "w") as f:
